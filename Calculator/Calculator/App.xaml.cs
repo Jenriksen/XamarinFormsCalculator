86a0e8c33472fc8ab2aaa85f95c72d3a3fbcdf05
Jesper Henriksen <jesp8234@edu.ucl.dk>
2020-03-04 07:19:11 +0100

    Add project files.

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var nav = new NavigationPage(new MainPage());
+
+            MainPage = nav;
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public int CurrentState { get; private set; } = 1;
+        public string MathOperator { get; private set; }
+        public double FirstNumber { get; private set; }
+        public double SecondNumber { get; private set; }
+
+        public void OnSelectNumber(object sender, EventArgs e)
+        {
+            if (CurrentState < 0)
+            {
+                // From flowchart
+                // state = state * -1
+                CurrentState = CurrentState * -1;
+            }
+
+            // From flowchart
+            // Set result label to number
+            //ResultText.Text = first 
+
+            Button numberPressed = (Button)sender;
+
+            if (CurrentState == 1)
+            {
+                FirstNumber = double.Parse(numberPressed.Text);
+            }
+            else
+            {
+                SecondNumber = double.Parse(numberPressed.Text);
+            }
+
+        }
+
+        public void OnSelectOperator(object sender, EventArgs e)
+        {
+            CurrentState = -2;
+            Button OperatorClicked = (Button)sender;
+            MathOperator = OperatorClicked.Text;
+        }
+
+        public void OnClear(object sender, EventArgs e)
+        {
+            CurrentState = 1;
+            FirstNumber = 0;
+            SecondNumber = 0;
+            ResultText.Text = "0";
+        }
+
+        public void OnCalculate(object sender, EventArgs e)
+        {
+            CurrentState = -1;
+            // ResultText.Text =
+
+
+        }
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnNavigateToNewPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ContentPage{});
+        }
+
+    }
+}
